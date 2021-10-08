38e752ab5674f74a9ae19ea0cd4ac9b93641ad4f
jplopesgj <jplopesgj@gmail.com>
2021-10-08 00:56:05 -0300

    Iniciando e criando Home page

---
+++ views/site/index.php
@@ -0,0 +1,60 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Get Healthy';
+?>
+<div class="background-home-runnig d-flex flex-column justify-content-center text-center h-100">
+
+    <h1 class="display-2">Get Healthy</h1>
+
+    <strong>
+    <h5 class="mb-4">O início da sua saúde.</h5>
+    </strong>
+
+
+    <div class="d-flex justify-content-center text-center">
+        <p><a class="btn btn-lg btn-info mr-3" href="http://dev.gethealthy.com.br/site/souprofissional">Já sou um profissional</a></p>
+        <p><a class="btn btn-lg btn-warning" href="http://dev.gethealthy.com.br/site/cadastrar">Quero me cadastrar</a></p>
+    </div>
+</div>
+
+<?php /*
+<div class="body-content">
+   
+        <div class="row">
+            <div class="col-lg-4">
+                <h2>Heading</h2>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur.</p>
+
+                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>Heading</h2>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur.</p>
+
+                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>Heading</h2>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur.</p>
+
+                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+            </div>
+        </div>
+
+    </div> 
+</div>
+*/ ?>
